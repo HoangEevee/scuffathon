bcb3d0c7e588be399db9dc4ff517d2440073bfad
HoangEevee <nguyenquochoang2806@gmail.com>
2023-04-01 13:17:15 +1100

    simple code to add gif

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,15 @@
+//This add a dancing gorilla at first paragraph "p"
+const gorillaImg = document.createElement("iframe")
+gorillaImg.src="https://giphy.com/embed/j65kA9WAUGdDwfPLBp"
 
+insertPoint = document.querySelector("p")
+insertPoint.appendChild(gorillaImg)
 
 // Replace every word with anything else
 for (tag of ["h1","h2","h3","h4","h5","p","li","td","caption","span","a"]){
     const text = document.querySelectorAll(tag)
     console.log(text)
     for (let i=0;i<text.length;i++) {
-        // console.log(text[i].innerHTML + "A")
         text[i].innerHTML = text[i].innerHTML + "AAAA"
     }
 }
