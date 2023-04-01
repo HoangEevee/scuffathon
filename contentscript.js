ed402df805f177a27cefbcc890255d1c8b59b42c
HoangEevee <nguyenquochoang2806@gmail.com>
2023-04-01 14:10:21 +1100

    add gorilla img to centre of screen

--- contentscript.js
+++ contentscript.js
@@ -1,9 +1,11 @@
 //This add a dancing gorilla at first paragraph "p"
-const gorillaImg = document.createElement("iframe")
-gorillaImg.src="https://giphy.com/embed/j65kA9WAUGdDwfPLBp"
+const gorillaImg = document.createElement("img")
+gorillaImg.src="https://i.pinimg.com/originals/f8/a3/85/f8a3856d260df0d7166e2688d2154ec3.png"
+gorillaImg.style="position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);width:30%;height:30%"
 
-insertPoint = document.querySelector("p")
+insertPoint = document.querySelector("body")
 insertPoint.appendChild(gorillaImg)
+
 
 // Replace every word with anything else
 for (tag of ["h1","h2","h3","h4","h5","p","li","td","caption","span","a"]){
